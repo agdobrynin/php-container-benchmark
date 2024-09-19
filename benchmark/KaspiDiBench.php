aadc711476fe15913910be38bf5f65f91a527f89
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-09-19 16:25:39 +0400

    feat: Add new package "kaspi/di-container" with optimization.

--- benchmark/KaspiDiBench.php
+++ benchmark/KaspiDiBench.php
@@ -18,10 +18,16 @@ class KaspiDiBench  extends AbstractContainer
 
     public function getContainer(): void
     {
+        $definitions = [
+            \Project\Generated\ServiceInterface::class => \Project\Generated\ServiceImplementation::class,
+            'some_alias' => \Project\Generated\Service6::class,
+        ];
+        $services = !empty(getenv('SERVICES')) ? getenv('SERVICES') : 100;
+        for ($i = 0; $i < $services; $i++) {
+            $definitions["Project\Generated\Service$i"];
+        }
+
         $this->container = (new DiContainerFactory())
-            ->make([
-                \Project\Generated\ServiceInterface::class => \Project\Generated\ServiceImplementation::class,
-                'some_alias' => \Project\Generated\Service6::class,
-            ]);
+            ->make($definitions);
     }
 }
